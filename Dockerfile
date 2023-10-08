81e873d3f6bf933957598c62dc5be02a8a391e7e
realTaki <taki.wang@ualberta.ca>
2023-10-08 08:42:42 +0000

    add env options to yml file

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN apt-get update && apt-get install -y git gcc && apt-get clean && \
     pip install openai uvicorn fastapi pandas pydantic-yaml pydantic tenacity qdrant-client tiktoken
 
 COPY main.py main.py
+COPY embedbase embedbase
 
-CMD ["embedbase"]
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -3,14 +3,31 @@ import uvicorn
 from embedbase import get_app
 from embedbase.embedding.openai import OpenAI
 from embedbase.database.qdrant_db import Qdrant
-from embedbase.settings import get_settings_from_file
-from embedbase.database.memory_db import MemoryDatabase
+from embedbase.settings import Settings
 
-settings = get_settings_from_file()
+settings = Settings()
+
+settings.api_type = os.environ.get("API_TYPE")
+
+settings.openai_api_key = os.environ.get("OPENAI_API_KEY")
+settings.openai_organization = os.environ.get("OPENAI_ORGANIZATION")
+
+settings.azure_api_key = os.environ.get("AZURE_API_KEY")
+settings.azure_api_base = os.environ.get("AZURE_API_BASE")
+settings.azure_api_version = os.environ.get("AZURE_API_VERSION")
+settings.azure_deployment_id = os.environ.get("AZURE_DEPLOYMENT_ID")
+
+settings.supabase_url = os.environ.get("SUPABASE_URL")
+settings.supabase_key = os.environ.get("SUPABASE_KEY")
+
+settings.qdrant_url = os.environ.get("QDRANT_URL")
+settings.qdrant_api_key = os.environ.get("QDRANT_API_KEY")
 
 # here we use openai to create embeddings and qdrant to store the data
-# app = get_app().use_embedder(OpenAI(os.environ["OPENAI_API_KEY"])).use_db(Qdrant()).run()
-app = get_app().use_embedder(OpenAI(settings)).use_db(MemoryDatabase()).run()
+app = get_app().use_embedder(OpenAI(settings)).use_db(Qdrant(
+    url=settings.qdrant_url,
+    api_key=settings.qdrant_api_key
+)).run()
 
 if __name__ == "__main__":
     uvicorn.run(app)
